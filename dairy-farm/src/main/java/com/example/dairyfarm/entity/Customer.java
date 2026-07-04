9a1632311a5f1f6d99fdd2e87e8841ee59f2bfe1
SaloniChikhalkar <salonichikhalkar1111@gmail.com>
2026-07-04 12:09:24 +0530

    customer module start

--- dairy-farm/src/main/java/com/example/dairyfarm/entity/Customer.java
+++ dairy-farm/src/main/java/com/example/dairyfarm/entity/Customer.java
@@ -1,5 +1,63 @@
 package com.example.dairyfarm.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(Long customerId, String customerName, String mobileNumber, String address) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.mobileNumber = mobileNumber;
+        this.address = address;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
